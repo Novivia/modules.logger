0b64cbb0bdbc232ea648248c9d9888aaa78d5a15
Mathieu M-Gosselin <mathieumg@atx33.com>
2014-05-19 17:27:14 -0400

    Version 0.1 of the logger, expects a label name when required.

---
+++ changelog.md
@@ -0,0 +1,6 @@
+# Versions
+
+## v0.1 - (19/05/2014)
+
+* Initial tagged version of the logger.
+* Logger now expects a label name when being required.
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var winston = require('winston');
 
+// Remove default (basic) console logger.
 winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, {
-    colorize: true,
-    timestamp: function() { return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''); }
-});
 
-module.exports = winston;
+// Provide a new logger each time we're called.
+var newLogger = function newLogger(name) {
+  return new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        prettyPrint: true,
+        colorize: true,
+        timestamp: function() { return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''); },
+        label: (name || undefined)
+      })
+    ]
+  });
+}
+
+module.exports = newLogger;
+
+// Temporary. For unupdated modules that don't instanciate logger explicitely.
+module.exports.info = winston.info;
